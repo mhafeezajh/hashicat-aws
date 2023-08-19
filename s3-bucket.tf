2fb4c84bc4b1b60379087cb9ef9fbb4cc875f007
mhafeezajh <mhafeezajh@gmail.com>
2023-08-19 23:02:44 +0000

    Added s3 bucket module-corrected prefix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "hafeez"
+  bucket_prefix = "hafeez"
   acl    = "private"
 
   versioning = {
